dbbf62a2451dbca0c00e17a14bbec5de8ae4810e
Andrew Branch <abranch@appddictionstudio.com>
2019-10-25 09:36:39 -0500

    testing multi-statement sql scripts.

--- liquibase/lb-project/sql/001_create_test_tables.sql
+++ liquibase/lb-project/sql/001_create_test_tables.sql
@@ -1,5 +1,8 @@
-create table AA_TEST_1 ( ID int not null, FNAME varchar(100) not null);
-create table AA_TEST_2 ( ID int not null, FNAME varchar(100) not null);
-create table AA_TEST_3 ( ID int not null, FNAME varchar(100) not null);
+create table AA_TEST_1 ( ID int not null, FNAME varchar(100) not null)
+go
+create table AA_TEST_2 ( ID int not null, FNAME varchar(100) not null)
+go
+create table AA_TEST_3 ( ID int not null, FNAME varchar(100) not null)
+go
 create table AA_TEST_4 ( ID int not null, FNAME varchar(100) not null)
 
