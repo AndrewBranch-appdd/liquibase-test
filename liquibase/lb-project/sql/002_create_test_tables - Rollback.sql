6518bdfe35cb66a661a2bdcc8b410eb3c8c3bbe1
Andrew Branch <abranch@appddictionstudio.com>
2019-10-25 15:59:28 -0500

    adding --formatted sql to test script.

---
+++ liquibase/lb-project/sql/002_create_test_tables - Rollback.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset Andrew:001_create_test_tables splitStatements:true endDelimiter:;
+Drop table AA_TEST_1;
+Drop table AA_TEST_2;
+Drop table AA_TEST_3;
+Drop table AA_TEST_4;
+
