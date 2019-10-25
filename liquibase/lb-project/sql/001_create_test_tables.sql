2e10210466ea8407b768205d59a6a1436348e181
Andrew Branch <abranch@appddictionstudio.com>
2019-10-25 10:07:10 -0500

    testing multi-statement sql scripts.

--- liquibase/lb-project/sql/001_create_test_tables.sql
+++ liquibase/lb-project/sql/001_create_test_tables.sql
@@ -1,11 +1,6 @@
-Statement s = c.createStatement() /
-String s1 = "create table AA_TEST_1 ( ID int not null, FNAME varchar(100) not null)" /
-String s2 = "create table AA_TEST_2 ( ID int not null, FNAME varchar(100) not null)" /
-String s3 = "create table AA_TEST_3 ( ID int not null, FNAME varchar(100) not null)" /
-String s4 = "create table AA_TEST_4 ( ID int not null, FNAME varchar(100) not null)" /
-
-s.addBatch(s1) /
-s.addBatch(s2) /     
-s.addBatch(s3) /
-s.addBatch(s4) /
-s.executeBatch()
+BEGIN
+create table AA_TEST_1 ( ID int not null, FNAME varchar(100) not null);
+create table AA_TEST_2 ( ID int not null, FNAME varchar(100) not null);
+create table AA_TEST_3 ( ID int not null, FNAME varchar(100) not null);
+create table AA_TEST_4 ( ID int not null, FNAME varchar(100) not null)
+END
